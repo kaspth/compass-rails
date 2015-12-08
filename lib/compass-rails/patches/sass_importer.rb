22f1ae12670a7a49e2a4479043f4b303f0136b03
Kasper Timm Hansen <kaspth@gmail.com>
2015-12-08 16:27:20 +0100

    Respect the arity of SassImporter initialize.

--- lib/compass-rails/patches/sass_importer.rb
+++ lib/compass-rails/patches/sass_importer.rb
@@ -9,7 +9,7 @@ klass.class_eval do
     # Use custom importer that knows about Sprockets Caching
     cache_store = Sprockets::SassCacheStore.new(context.environment)
     paths  = context.environment.paths.map { |path| CompassRails::SpriteImporter.new(context, path) }
-    paths += context.environment.paths.map { |path| self.class.parent::SassImporter.new(context, path) }
+    paths += context.environment.paths.map { |path| sass_importer(context, path) }
     paths += ::Rails.application.config.sass.load_paths
 
 
@@ -18,7 +18,7 @@ klass.class_eval do
       :line => line,
       :syntax => syntax,
       :cache_store => cache_store,
-      :importer => self.class.parent::SassImporter.new(context, context.pathname),
+      :importer => sass_importer(context, context.pathname),
       :load_paths => paths,
       :sprockets => {
         :context => context,
@@ -32,5 +32,12 @@ klass.class_eval do
     context.__LINE__ = e.sass_backtrace.first[:line]
     raise e
   end
-end    
+
+  private
+    def sass_importer(context, path)
+      self.class.parent::SassImporter.new(context, path)
+    rescue
+      self.class.parent::SassImporter.new(path)
+    end
+end
 
